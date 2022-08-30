beb3b22c7561c45c978f148d8ca09cbb6410ca65
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2022-08-30 10:49:53 +0200

    added timeout

--- model/framework/calculate.py
+++ model/framework/calculate.py
@@ -2,6 +2,8 @@ import csv
 import sys
 from rdkit import Chem
 from mordred import Calculator, descriptors
+from timeout_decorator import timeout
+import pandas as pd
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
@@ -13,8 +15,25 @@ with open(infile, "r") as f:
     for r in reader:
         smiles += [r[0]]
 
+TIMEOUT_SEC=3
+
 calc = Calculator(descriptors, ignore_3D=True)
 
-df = calc.pandas([Chem.MolFromSmiles(smi) for smi in smiles])
+@timeout(TIMEOUT_SEC)
+def one_molecule(mol):
+    return calc(mol)
+
+
+columns = list(calc.pandas([Chem.MolFromSmiles("CCCC")]).columns)
+
+R = []
+for smi in smiles:
+    try:
+        r = one_molecule(Chem.MolFromSmiles(smi))
+    except:
+        r = [None for _ in range(len(columns))]
+    R += [r]
+
+df = pd.DataFrame(R, columns=columns)
 
 df.to_csv(outfile, index=False)
