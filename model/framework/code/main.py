ff2d0bcf520f7d0b7f7b89957171fae3548f4170
GemmaTuron <gemma@ersilia.io>
2025-06-30 11:21:11 +0200

    update model

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -21,7 +21,7 @@ with open(infile, "r") as f:
     for r in reader:
         smiles += [r[0]]
 
-TIMEOUT_SEC=3
+TIMEOUT_SEC=60
 
 calc = Calculator(descriptors, ignore_3D=True)
 
@@ -37,22 +37,23 @@ def convert_to_float(df):
                 try:
                     df.at[index, col] = float(value)
                 except ValueError:
-                    #print(f"Non-numeric value found at row {index}, column '{col}, {value}'")
                     df.at[index, col] = np.nan
-    df = df.applymap(lambda x: np.nan if pd.isna(x) else x)
+    df = df.map(lambda x: np.nan if pd.isna(x) else x)
     return df
 
-
-#columns = calc._name_dict.keys()
 columns = list(calc.pandas([Chem.MolFromSmiles("CCCC")]).columns)
 
 R = []
-for smi in smiles:
+invalid_idxs = []
+for idx, smi in enumerate(smiles):
     try:
         r = one_molecule(Chem.MolFromSmiles(smi))
     except:
         r = [None for _ in range(len(columns))]
+        invalid_idxs += [idx]
     R += [r]
+
+invalid_idxs = set(invalid_idxs)
 
 cols_to_drop = joblib.load(os.path.join(checkpoints_dir, "cols_to_drop.pkl"))
 imputer = joblib.load(os.path.join(checkpoints_dir,"imputer.pkl"))
@@ -64,10 +65,12 @@ R = imputer.transform(R)
 
 cols = [c.lower() for c in columns if c not in cols_to_drop]
 
-#TODO: we are now imputing molecules that might be all NaNs originally
 
 with open(outfile, "w") as f:
     writer = csv.writer(f)
     writer.writerow(cols)
-    for r in R:
-        writer.writerow(r)
+    for i, r in enumerate(R):
+        if i in invalid_idxs:
+            writer.writerow([None for _ in range(len(cols))])
+        else:
+            writer.writerow(r)
